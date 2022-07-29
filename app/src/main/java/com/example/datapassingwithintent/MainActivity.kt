5f05c2df12e6bfec205972323be6d326ca051ef0
Parampreet-Kaur002 <kaurparampreet002@gmail.com>
2022-07-29 21:14:34 +0530

    Changes done and errors are resolved
    done

--- app/src/main/java/com/example/datapassingwithintent/MainActivity.kt
+++ app/src/main/java/com/example/datapassingwithintent/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.datapassingwithintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var switch1: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        switch1 = findViewById(R.id.switch1)
+        switch1.setOnCheckedChangeListener { compoundButton, isOn ->
+            if(isOn){
+                
+            }else{}
+        }
     }
 }
